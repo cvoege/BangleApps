f56beb10658ec01f8795ced8fc32d3f6cbcef79c
Colton Voege <colton@beacons.ai>
2023-08-12 00:40:36 -0400

    Add otterclock

---
+++ apps/otterclock/app-icon.js
@@ -0,0 +1,5 @@
+require("heatshrink").decompress(
+  atob(
+    "iMRwkC///+EwAAIFBAAMAAYXwh4CBgYMD+YNBB4YRD+E/h8zgYDBEYMD+EfgEPiHzn8DAQPwDgMzEQM/+YeBFIP/mAxB+cQmETAgMPGREQI4JQCiHwiMTgEzDwMRiEBNQNAmFBAgMIwERokDglBiGIxGAgEDmaBBA4IAPwA="
+  )
+);
